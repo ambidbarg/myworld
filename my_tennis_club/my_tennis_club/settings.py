cedfcb23752a299292bdda49900a3f8220bce564
ambidbarg <am.bidbarg@gmail.com>
2023-06-15 12:07:41 +0330

    Revert "Revert "change DEBUG = False""
    This reverts commit ffb5259cba7231a9ffcc75d5a4cf3914aa24a86f.

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n#vio4((qt&)^(=&zqyex7qc!+hdui1%wqsie3^_04c3fv67ae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
